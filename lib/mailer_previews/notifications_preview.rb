17d741a9e312ff6fce6f22cfc82093043d2086c7
Gulnaz Kassymova <gulnazkassym@gmail.com>
2020-04-18 15:16:16 +0600

    fixup! Email newsletters

--- lib/mailer_previews/notifications_preview.rb
+++ lib/mailer_previews/notifications_preview.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
-include EventsHelper
 
 class NotificationsPreview < ActionMailer::Preview
+  include EventsHelper
+
   def notify_about_new_event
-    NotificationsMailer.notify_about_new_event(Admin.pluck(:email), Event.first.id)
+    NotificationsMailer.notify_about_new_event(
+      Admin.pluck(:email), Event.first.id
+    )
   end
 
   def send_weekly_newsletter
     email_list = Subscriber.pluck(:email)
     today = Date.today
-    NotificationsMailer.with(email_list: email_list, week: today..today + 7.days).send_weekly_newsletter
+    NotificationsMailer.with(
+      email_list: email_list, week: today..today + 7.days
+    ).send_weekly_newsletter
   end
 end
--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Subscriber, type: :model do
   describe 'validates email' do
     it { should validate_presence_of(:email) }
-    it { should validate_uniqueness_of(:email) }
+
+    subject { create(:subscriber) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
   end
 end
